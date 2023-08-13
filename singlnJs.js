2d1b39835372e56d31bb6fa6766bbe78b8e186f3
cano_ing <josebulac960@gmail.com>
2023-08-13 10:59:45 -0500

    china

---
+++ singlnJs.js
@@ -0,0 +1,14 @@
+function ojo() {
+   
+    
+    if (Password.type == 'password') {
+        Password.type = 'text';
+    } else if (Password.type == 'text') {
+        Password.type = 'password';
+    }
+}
+function ir(){
+
+    window.location = "main.html"
+    
+}
