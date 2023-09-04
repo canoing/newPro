194c721a8e599c66320ed84f2c7ebc9238fb4ded
cano_ing <josebulac960@gmail.com>
2023-09-04 15:33:24 -0500

    escucha

---
+++ scp/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+
+const mongo_url =  `mongodb://${process.env.NOTES_APP_MONGODB_HOST}/${process.env.NOTES_APP_MONGODB_DATABASE}`;
+
+
+
+mongoose.connect(mongo_url, {
+        
+
+})
+    .then(db => console.log("escucho"))
+    .catch(err => console.log(err + "sordo"))
+
--- scp/index.js
+++ scp/index.js
@@ -1,5 +1,9 @@
-const app = require("./serve")
+require("dotenv").config()
 
+const app = require("./serve")
+require("./database")
+console.log(process.env.xf)
+console.log(`mongodb://${process.env.NOTES_APP_MONGODB_HOST}/${process.env.NOTES_APP_MONGODB_DATABASE}`)
 
 app.listen(app.get("port"), () => {
   console.log(`Server is running on port: `, app.get("port"));
