b104afa1f739dcb0a42ea9b9ce02d0d18b64de78
cano_ing <josebulac960@gmail.com>
2023-09-24 17:10:26 -0500

    temporal

--- scp/serve.js
+++ scp/serve.js
@@ -1,10 +1,25 @@
  const express = require('express')
 const path = require("path")
+const exphbs = require('express-handlebars');
 // iniciar 
 const app = express()
 // configuracion 
 app.set("port",process.env.PORT || 4000)
-app.set("views",path.join(__dirname +  "views"))
+app.set("views", path.join(__dirname, "views"));
+
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'                
+}));
+app.set('view engine', '.hbs'); // Establecer Handlebars como el motor de plantillas predeterminado
+app.set('views', path.join(__dirname, 'views'));
+// rutas
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 // peticion 
 app.use(express.urlencoded({extended:false}))
 
@@ -12,13 +27,7 @@ app.use(express.urlencoded({extended:false}))
 
 
 
-// rutas
-app.get('/', (req,res ) => {
 
-    res.sendFile(path.join(__dirname, 'views', 'user.html'));
-
-}
-)
 
 // estico
 app.use(express.static(path.join(__dirname,"public")))
